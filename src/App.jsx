fa732a20d9667798c9a987ac93fbb947fa5b3127
SHIVAM SHARMA <128138567+shivamsharmadac@users.noreply.github.com>
2023-06-16 13:29:13 +0530

    First

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./Chicken.css"
+import './App.css'
+import Chicken from './Chicken'
+
+function App() {
+ 
+  return (
+    <>
+    <h1>Welcome to shark tank</h1>
+<Chicken/>
+<Chicken/>
+<Chicken/>
+    </>
+  );
+}
+export default App;
---
+++ src/Chicken.jsx
@@ -0,0 +1,6 @@
+import "./Chicken.css"
+function Chicken(){
+    return <p className="Chicken">puck puck puck</p>
+}
+
+export  default Chicken;
